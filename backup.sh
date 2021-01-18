251c738756fcdf33b7f4bc5bc4377dc7ab6081b4
Andre Barsotti <andre.barsotti@accelera.com.br>
2021-01-18 17:15:15 -0300

    Exemplo Script backup

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Parametros
+#BACKUP_SOURCE="/mnt/dados"
+#BACKUP_DESTIN="/mnt/backup"
+#BACKUP_NAME="link-dados"
+
+#Montando o nome dos arquivos
+DATESTR=$(date +"%Y-%m-%d")
+
+#Processo do fullbackup
+echo "$(date +"%H:%M:%S") Iniciando o backup..." > $BACKUP_DESTIN/log-$DATESTR.log
+#echo "$(date +"%H:%M:%S") Apagando arquivos antigos..." >> $BACKUP_DESTIN/log-$DATESTR.log
+#rm -fv $BACKUP_DESTIN/$BACKUP_NAME*.* >>  $BACKUP_DESTIN/log-$DATESTR.log
+echo "$(date +"%H:%M:%S") Fazendo o backup..." >> $BACKUP_DESTIN/log-$DATESTR.log
+tar -g $BACKUP_DESTIN/$BACKUP_NAME.snar -cpf $BACKUP_DESTIN/$BACKUP_NAME-$(date +"%Y%m%d%H%M%S").tar $BACKUP_SOURCE >> $BACKUP_DESTIN/log-$DATESTR.log
+echo "$(date +"%H:%M:%S") Fim do backup..." >> $BACKUP_DESTIN/log-$DATESTR.log
