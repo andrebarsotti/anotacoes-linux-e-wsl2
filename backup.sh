1e0b10f73d7f8f20360c0dac186ebd85df683eba
Andre Barsotti <andre.barsotti@accelera.com.br>
2021-01-19 09:59:16 -0300

    Melhorias no backup

--- backup.sh
+++ backup.sh
@@ -9,9 +9,13 @@ BACKUP_NAME="link-dados"
 DATESTR=$(date +"%Y-%m-%d")
 
 #Processo do backup incremental com tar.
-echo "$(date +"%H:%M:%S") Iniciando o backup..." > $BACKUP_DESTIN/log-$DATESTR.log
-#echo "$(date +"%H:%M:%S") Apagando arquivos antigos..." >> $BACKUP_DESTIN/log-$DATESTR.log
-#rm -fv $BACKUP_DESTIN/$BACKUP_NAME*.* >>  $BACKUP_DESTIN/log-$DATESTR.log
-echo "$(date +"%H:%M:%S") Fazendo o backup..." >> $BACKUP_DESTIN/log-$DATESTR.log
-tar -g $BACKUP_DESTIN/$BACKUP_NAME.snar -cpf $BACKUP_DESTIN/$BACKUP_NAME-$(date +"%Y%m%d%H%M%S").tar $BACKUP_SOURCE >> $BACKUP_DESTIN/log-$DATESTR.log
-echo "$(date +"%H:%M:%S") Fim do backup..." >> $BACKUP_DESTIN/log-$DATESTR.log
+echo "$(date +"%Y-%m-%d %H:%M:%S") Iniciando o backup..." >> /var/local/backup-files.log
+#Faz um full-backup as segundas e incremental nos outros dias.
+if [ $(date +"%u") -eq "1" ]
+then
+  echo "$(date +"%Y-%m-%d %H:%M:%S") Apagando arquivos antigos..." >> /var/local/backup-files.log
+  rm -fv $BACKUP_DESTIN/$BACKUP_NAME*.* >>  /var/local/backup-files.log
+fi
+echo "$(date +"%Y-%m-%d %H:%M:%S") Fazendo o backup..." >> /var/local/backup-files.log
+tar -g $BACKUP_DESTIN/$BACKUP_NAME.snar -cpf $BACKUP_DESTIN/$BACKUP_NAME-$(date +"%Y%m%d%H%M%S").tar $BACKUP_SOURCE >> /var/local/backup-files.log
+echo "$(date +"%Y-%m-%d %H:%M:%S") Fim do backup..." >> /var/local/backup-files.log
